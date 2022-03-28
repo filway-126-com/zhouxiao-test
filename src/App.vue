d7ba59b0eae0f3dda14b7ba45fe6dd726af98798
filway <filway@126.com>
2022-03-28 23:41:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    vue3 template 标准模板 (1.0.0)
+    <div style="height: 300px; width: 300px; background: pink">新增div1</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/zhouxiao-test/'
+}
