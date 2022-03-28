e4319d16a961619a7df9e084af424391306e2afa
filway <filway@126.com>
2022-03-28 23:43:52 +0800

    新增div2

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     vue3 template 标准模板 (1.0.0)
     <div style="height: 300px; width: 300px; background: pink">新增div1</div>
+    <div style="height: 300px; width: 300px; background: skyblue">新增div2</div>
   </div>
 </template>
 
