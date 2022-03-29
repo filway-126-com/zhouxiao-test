d61cebc029fdb04ef630fcbe559b23ba688c497e
filway <filway@126.com>
2022-03-29 20:02:27 +0800

    新增代码片段

--- vue3-page-test/index.vue
+++ vue3-page-test/index.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="vue3页面模板测试">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <vue2code></vue2code>
     <el-input></el-input>
     <el-button>提交</el-button>
   </div>
 </template>
 
 <script>
+import vue2code from './components/vue2code/index.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import clonedeep from 'lodash.clonedeep'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    vue2code,
   },
   created() {
     const o = {
       a: 1,
-      b: 2
+      b: 2,
     }
-    const o2 = clonedeep(o);
+    const o2 = clonedeep(o)
     console.log(o === o2)
-  }
+  },
 }
 </script>
 
